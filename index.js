49118ecdae6f014dc6563e9f4d9aba209e9a226a
Marco Alfonsi <malfonsi@ibc.it>
2024-07-05 14:31:24 +0200

    Added brands endpoint

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require("express");
-const { search } = require("./scrapeLogic");
+const { search, brands } = require("./scrapeLogic");
 const app = express();
 
 const PORT = process.env.PORT || 4000;
 
 app.get("/search", (req, res) => {
   search(req, res);
+});
+
+app.get("/brands", (req, res) => {
+  brands(req, res);
 });
 
 // app.get("/scrape", (req, res) => {
--- scrapeLogic.js
+++ scrapeLogic.js
@@ -91,4 +91,64 @@ const search = async (req, res) => {
   }
 };
 
-module.exports = { search };
+const brands = async (req, res) => {
+  const browser = puppeteer.launch({
+    headless: "new",
+    defaultViewport: false,
+    args: [
+      "--disable-setuid-sandbox",
+      "--no-sandbox",
+      "--single-process",
+      "--no-zygote",
+    ],
+    executablePath:
+      process.env.NODE_ENV === "production"
+        ? process.env.PUPPETEER_EXECUTABLE_PATH
+        : puppeteer.executablePath(),
+  });
+  try {
+    const page = await browser.newPage();
+    page.setDefaultTimeout(3*60*1000);
+    await page.setViewport({width: 640, height:480})
+
+    // Go to the target website
+    await page.goto(`https://www.vinted.it/catalog`, { waitUntil: 'networkidle2' });
+    await page.waitForNavigation();
+
+    const rejectCookie = await page.waitForSelector('#onetrust-reject-all-handler');
+    if(rejectCookie) {
+      await rejectCookie.click();
+    }
+
+    await page.waitForSelector('button[data-testid="trigger"]'); //Filters
+    await page.evaluate(() => {
+      document.querySelector('button[data-testid="trigger"]').click();
+    });
+
+    await page.waitForSelector('[data-testid="modal"]>div:nth-child(2)>div:nth-child(5)>div'); // Brands
+    await page.evaluate(() => {
+      document.querySelector('[data-testid="modal"]>div:nth-child(2)>div:nth-child(5)>div').click();
+    });
+
+    // Evaluate the page content and extract the desired information
+    await page.waitForSelector('ul.pile>li span.web_ui__Text__title'); // Brand list
+    const items = await page.evaluate(() => {
+      const elements = document.querySelectorAll('ul.pile>li>div>div');
+      return Array.from(elements).map(element => ({
+        name: element.children[0].children[0].children[0].children[0].textContent,
+        id: element.id.slice(element.id.lastIndexOf("-")+1, element.id.length)
+      }));
+    });
+
+    // Send the extracted information as JSON
+    res.json(items);
+  }
+  catch (e) {
+    console.error(e);
+    res.send(`Something went wrong while running Puppeteer: ${e}`);
+  } finally {
+    await browser.close();
+  }
+}
+
+module.exports = { search, brands };
