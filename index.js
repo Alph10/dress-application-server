0a12224ab4db27005a166a8b04555411614ab6a6
Marco Alfonsi <malfonsi@ibc.it>
2024-07-05 14:49:42 +0200

    Test remove search

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require("express");
-const { search, brands } = require("./scrapeLogic");
+const { brands } = require("./scrapeLogic");
 const app = express();
 
 const PORT = process.env.PORT || 4000;
 
-app.get("/search", (req, res) => {
-  search(req, res);
-});
+// app.get("/search", (req, res) => {
+//   search(req, res);
+// });
 
 app.get("/brands", (req, res) => {
   brands(req, res);
--- scrapeLogic.js
+++ scrapeLogic.js
@@ -27,68 +27,68 @@ require("dotenv").config();
 //   }
 // };
 
-const search = async (req, res) => {
-  const browser = await puppeteer.launch({
-    args: [
-      "--disable-setuid-sandbox",
-      "--no-sandbox",
-      "--single-process",
-      "--no-zygote",
-    ],
-    executablePath:
-      process.env.NODE_ENV === "production"
-        ? process.env.PUPPETEER_EXECUTABLE_PATH
-        : puppeteer.executablePath(),
-  });
-  try {
-    // Get query
-    const query = req.query;
+// const search = async (req, res) => {
+//   const browser = await puppeteer.launch({
+//     args: [
+//       "--disable-setuid-sandbox",
+//       "--no-sandbox",
+//       "--single-process",
+//       "--no-zygote",
+//     ],
+//     executablePath:
+//       process.env.NODE_ENV === "production"
+//         ? process.env.PUPPETEER_EXECUTABLE_PATH
+//         : puppeteer.executablePath(),
+//   });
+//   try {
+//     // Get query
+//     const query = req.query;
 
-    var queryString = "";
-    Object.entries(query).forEach(queryElement => {
-      if (Array.isArray(queryElement[1])) {
-        queryElement[1].forEach(element => {
-          queryString += (queryElement[0] + "[]=" + element + "&");
-        })
-      }
-      else {
-        queryString += (queryElement[0] + "=" + queryElement[1] + "&");
-      }
-    });
+//     var queryString = "";
+//     Object.entries(query).forEach(queryElement => {
+//       if (Array.isArray(queryElement[1])) {
+//         queryElement[1].forEach(element => {
+//           queryString += (queryElement[0] + "[]=" + element + "&");
+//         })
+//       }
+//       else {
+//         queryString += (queryElement[0] + "=" + queryElement[1] + "&");
+//       }
+//     });
 
-    console.log("/search", queryString);
+//     console.log("/search", queryString);
 
-    const page = await browser.newPage();
-    page.setDefaultTimeout(2*60*1000);
+//     const page = await browser.newPage();
+//     page.setDefaultTimeout(2*60*1000);
 
-    // Go to the target website
-    await page.goto(`https://www.vinted.it/catalog?${queryString}`, { waitUntil: 'networkidle2' });
+//     // Go to the target website
+//     await page.goto(`https://www.vinted.it/catalog?${queryString}`, { waitUntil: 'networkidle2' });
 
-    // Wait for a specific element to ensure all data has loaded
-    await page.waitForSelector('[data-testid^="product-item"]');
+//     // Wait for a specific element to ensure all data has loaded
+//     await page.waitForSelector('[data-testid^="product-item"]');
  
-    // Evaluate the page content and extract the desired information
-    const items = await page.evaluate(() => {
-      const elements = document.querySelectorAll('div.new-item-box__container[data-testid^=product-item-id]');
-      return Array.from(elements).map(element => ({
-        title: element.children[1].children[0].children[0].children[0].alt.trim().split(", ")[0],
-        price: element.children[1].children[0].children[0].children[0].alt.trim().split(", ")[1],
-        image: element.children[1].children[0].children[0].children[0].src,
-        // Double href so that it works when the a tag is in the position 1 or 2
-        link: element.children[1].children[1].href,
-        link: element.children[1].children[2].href
-      }));
-    });
+//     // Evaluate the page content and extract the desired information
+//     const items = await page.evaluate(() => {
+//       const elements = document.querySelectorAll('div.new-item-box__container[data-testid^=product-item-id]');
+//       return Array.from(elements).map(element => ({
+//         title: element.children[1].children[0].children[0].children[0].alt.trim().split(", ")[0],
+//         price: element.children[1].children[0].children[0].children[0].alt.trim().split(", ")[1],
+//         image: element.children[1].children[0].children[0].children[0].src,
+//         // Double href so that it works when the a tag is in the position 1 or 2
+//         link: element.children[1].children[1].href,
+//         link: element.children[1].children[2].href
+//       }));
+//     });
 
-    // Send the extracted information as JSON
-    res.json(items);
-  } catch (e) {
-    console.error(e);
-    res.send(`Something went wrong while running Puppeteer: ${e}`);
-  } finally {
-    await browser.close();
-  }
-};
+//     // Send the extracted information as JSON
+//     res.json(items);
+//   } catch (e) {
+//     console.error(e);
+//     res.send(`Something went wrong while running Puppeteer: ${e}`);
+//   } finally {
+//     await browser.close();
+//   }
+// };
 
 const brands = async (req, res) => {
   const browser = puppeteer.launch({
@@ -152,4 +152,4 @@ const brands = async (req, res) => {
   }
 }
 
-module.exports = { search, brands };
+module.exports = { brands };
